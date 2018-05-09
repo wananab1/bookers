0a91dc3358194bd9a970fdf0fac1a59288e6a168
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-09 18:53:25 +0900

    フラッシュメッセージ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,11 @@ class PostsController < ApplicationController
 
 	def create
 		post = Post.new(post_params)
-		post.save
-		redirect_to posts_path
+
+		if post.save
+			redirect_to posts_path
+			flash[:notice] = "投稿を作成しました"
+		end
 	end
 
 	def show
@@ -24,13 +27,17 @@ class PostsController < ApplicationController
 
 	def update
 		post = Post.find(params[:id])
-		post.update(post_params)
-		redirect_to post_path(post.id)
+
+		if post.update(post_params)
+			flash[:notice] = "投稿を編集しました"
+			redirect_to post_path(post.id)
+		end
 	end
 
 	def destroy
 		post = Post.find(params[:id])
 		post.destroy
+		flash[:notice] = "投稿を削除しました"
 		redirect_to posts_path
 	end
 
