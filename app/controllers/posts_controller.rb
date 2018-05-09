a0c64987c13f797f8424ea526fee2b4e7f3d0dd3
Shota Watanabe <wtnbshut.mij@gmail.com>
2018-05-09 13:54:38 +0900

    make routing and controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+	def top
+	end
+
+	def new
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
+
+end
